a3036aed3124f4ce3e5be8589dfa985bffe5f6ac
rismaamaliyah <rismaamaliyah2@gmail.com>
2025-06-24 20:54:27 +0700

    Passed calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((result, num) => result + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((result, num) => result * num, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+  for (let i = 2; i <= num; i++){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line

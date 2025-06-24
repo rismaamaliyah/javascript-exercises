e83925ccf7e843ab1c6263b3ce54aa4ccff8bf1a
rismaamaliyah <rismaamaliyah2@gmail.com>
2025-06-24 20:32:32 +0700

    Passed temperature conversion exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (f) {
+  let result = 0;
+  result = (f - 32) * 5 / 9;
+  return parseFloat(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (c) {
+  let result = 0;
+  result = (c * 9 / 5) + 32;
+  return parseFloat(result.toFixed(1));
 };
 
 // Do not edit below this line

535c44d5539420945c231e547c04da360bb4c035
rismaamaliyah <rismaamaliyah2@gmail.com>
2025-06-24 18:11:22 +0700

    Passed repeat string exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (word, num) {
+    let result = "";
+    if (num < 0) {
+        result = "ERROR";
+    } else if (num > 0) {
+        for (let i = 0; i < num; i += 1){
+            result += word;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line

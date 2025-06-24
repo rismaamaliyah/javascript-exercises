aa279af50679635eef02d073ed7f62ad80dd6efa
rismaamaliyah <rismaamaliyah2@gmail.com>
2025-06-24 19:03:18 +0700

    Passed remove from array section

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...elementToRemove) {
+    let newArr = [];
+
+    for (let i = 0; i < arr.length; i++){
+        if (!elementToRemove.includes(arr[i])) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line

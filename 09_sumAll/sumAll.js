e01f686465c20016ecedb7cd4dc111563c203e72
rismaamaliyah <rismaamaliyah2@gmail.com>
2025-06-24 19:23:42 +0700

    Passed sum all exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (start, stop) {
+    let result = 0;
 
+    if (typeof start !== 'number' || typeof stop !== 'number') {
+        return "ERROR";
+    } 
+
+    if (!Number.isInteger(start) || !Number.isInteger(stop) || start < 0 || stop < 0) {
+        return "ERROR";
+    }
+
+    if (start > stop) {
+        [start, stop] = [stop, start];
+    }
+
+    while (start <= stop) {
+        result += start;
+        start++;
+    }
+    return result;
 };
 
 // Do not edit below this line

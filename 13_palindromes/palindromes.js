c12169200e63a4e967dbbb7f8116d3de1a74270b
rismaamaliyah <rismaamaliyah2@gmail.com>
2025-06-25 22:00:00 +0700

    Passed palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    const cleaned = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let left = 0;
+    let right = cleaned.length - 1;
 
+    while (left < right) {
+        if (cleaned[left] !== cleaned[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
 };
 
 // Do not edit below this line

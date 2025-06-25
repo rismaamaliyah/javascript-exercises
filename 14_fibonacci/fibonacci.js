444241cfec7b024cff7c9d1b1e5baf1808eab6c6
rismaamaliyah <rismaamaliyah2@gmail.com>
2025-06-25 22:21:10 +0700

    Passed fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let fib = [];
 
+    fib[0] = 1;
+    fib[1] = 1;
+
+    if (typeof n === 'string') {
+        n = Number(n);
+    }
+
+    if (n === 0) {
+        return 0;
+    } else if (n < 0) {
+        return "OOPS";
+    } else {
+        for (let i = 2; i <= n - 1; i++){
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        return fib[n - 1];
+    }
 };
 
 // Do not edit below this line

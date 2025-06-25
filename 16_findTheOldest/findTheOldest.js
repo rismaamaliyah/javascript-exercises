592c10ef1a63b701ee3320f5fb17f3f6cfd52d4e
rismaamaliyah <rismaamaliyah2@gmail.com>
2025-06-25 23:01:14 +0700

    Passed find the oldest exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let currentYear = new Date().getFullYear();
 
+    return people.reduce((oldest, person) => {
+        let age = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : currentYear - person.yearOfBirth;
+        let oldestAge = oldest.yearOfDeath ? oldest.yearOfDeath - oldest.yearOfBirth : currentYear - oldest.yearOfBirth;
+        return age > oldestAge ? person : oldest;
+    }, people[0]);
 };
 
 // Do not edit below this line

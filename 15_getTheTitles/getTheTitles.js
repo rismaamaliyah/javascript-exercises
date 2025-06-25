7ab423393b429d4be6d301da6401c1ab49010379
rismaamaliyah <rismaamaliyah2@gmail.com>
2025-06-25 22:33:07 +0700

    Passed get the titles exercise

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    let titles = [];
 
+    for (ar of arr) {
+        titles.push(ar.title);
+    }
+
+    return titles;
 };
 
 // Do not edit below this line
